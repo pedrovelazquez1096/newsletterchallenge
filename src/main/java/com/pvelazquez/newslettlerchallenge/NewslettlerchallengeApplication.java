43ee69580825b6f8865a2447b22a0c793126f419
Pedro Eduardo Velazquez Gomez <pedrovelazquez.1096@gmail.com>
2024-10-31 22:24:25 -0600

    First commit

---
+++ src/main/java/com/pvelazquez/newslettlerchallenge/NewslettlerchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.pvelazquez.newslettlerchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewslettlerchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewslettlerchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvelazquez/newslettlerchallenge/NewslettlerchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvelazquez.newslettlerchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewslettlerchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
